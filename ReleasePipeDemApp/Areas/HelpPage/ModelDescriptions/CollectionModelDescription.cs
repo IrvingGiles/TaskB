efbc48a8998fa3b694bd60e621878ba9c880fcd6
Irving <irvinggiles10@gmail.com>
2018-09-20 18:15:10 +1000

    'defualt implementation'

---
+++ ReleasePipeDemApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ReleasePipeDemApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ReleasePipeDemApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ReleasePipeDemApp.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
